c24f61129222f104791be9d344926e4128d7f8af
Oscar Newman <newman.oscar@gmail.com>
2019-12-09 01:22:52 -0500

    no threaded loading

--- koalanet/KoalaNet.py
+++ koalanet/KoalaNet.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
                                     root_dir=args.trainwith,
                                     crop=512
                                     )
-    train_data_loader = data.DataLoader(train_dataset, batch_size=args.batch, shuffle=True, num_workers=8)
+    train_data_loader = data.DataLoader(train_dataset, batch_size=args.batch, shuffle=True)
 
     test_dataset = RawImageDataset(manifest_csv=os.path.join(args.testwith, 'manifest.csv'),
                                    root_dir=args.testwith,
